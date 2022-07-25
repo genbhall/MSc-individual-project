3b405f21430a2783572550efc1f3f87ff7e6f1d5
Gen Hall <gbh21@texel33.doc.ic.ac.uk>
2022-07-25 11:49:27 +0100

    full updates

--- model/test_model.py
+++ model/test_model.py
@@ -2,8 +2,6 @@ from orion import Orion
 import pandas as pd
 from output_utils.utils import plot
 from supporting_func.key_variables import all_activities
-from config.model import hyperparameters
-from orion.primitives.tadgan import TadGAN
 
 if __name__ == "__main__":
     
@@ -11,14 +9,30 @@ if __name__ == "__main__":
     df = pd.read_csv(filename)
     df = df[all_activities]
 
-    orion = Orion(pipeline='config/tadgan.json')
-    anomalies = orion.fit_detect(df)
-    plot(df,[anomalies])
-    print(anomalies.head(10))
 
+    orion = Orion(pipeline='config/tadgan.json')
+    
+    anomalies = orion.fit_detect(df)
+    # plot(df,[anomalies])
+    print(anomalies.head(10))
 
 '''
 from supporting_func.supporting_func import save_model, load_model
 Notes: pickle - to train model, use orion.fit(df). - this will train the latent space. Then can save_model(orion)
 when you load, save it as a new object. Then run orion.detect(df) to find said anomalies (as anomalies)
+'''
+
+'''
+hyperparameter tuning - can set variables here
+
+parameters = {
+    "mlprimitives.custom.timeseries_preprocessing.time_segments_aggregate#1": {
+        "interval": 3600 # hour level
+    },
+    'orion.primitives.tadgan.TadGAN#1': {
+        'epochs': 15,
+        'latent_dim': 40,
+        'dense_units': 40,
+    }
+}
 '''
