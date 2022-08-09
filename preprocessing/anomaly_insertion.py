3e82e1ee1e287f114fc0d731a5f7388943212791
Gen Hall <genbhall@gmail.com>
2022-08-09 12:25:17 +0100

    anomaly insertion

--- preprocessing/anomaly_insertion.py
+++ preprocessing/anomaly_insertion.py
@@ -1,7 +1,58 @@
 from global_variables.global_variables import filename_proc, all_activities, data_name, interval
 import pandas as pd
 
-#sleeps +5 hours longer
+#sleeps out of bed until 3am - only 4.20h sleep in bed - smaller anom
+#brush teeth early then sleep out of bed (3.83h sleep) - big anom
+#sleeps in until 10am - big anom
+#sleeps out of bed until 1:50, then starts watching TV from 4:40 - big anom
+#slept 9.4h - big anom
+#slept 8.72h more than 1std
+#slept 4.13h - should be lower rating
+#slept 8.37h - should be lower rating 
+
+existing_anomalies_hh101 = {
+    'start': [
+        '2012-08-22 00:00:00',
+        '2012-08-24 03:47:00',
+        '2012-08-29 07:00:00',
+        '2012-08-30 00:00:00',
+        '2012-09-01 07:00:00',
+        '2012-09-04 00:00:00',
+        '2012-09-14 00:00:00',
+        '2012-09-15 00:00:00',
+    ],
+    'end': [
+        '2012-08-22 08:00:00',
+        '2012-08-24 07:00:00',
+        '2012-08-29 10:00:00',
+        '2012-08-30 05:00:00',
+        '2012-09-01 10:00:00',
+        '2012-09-04 09:00:00',
+        '2012-09-14 08:00:00',
+        '2012-09-15 08:00:00',
+    ],
+}
+
+#Additional Synthetic Sleep Anomalies To include ------------------------------------------------------------
+
+#Random 2.5h nap
+sleep_random_anomaly = {
+    'start': ['2012-09-08 14:00:00'],
+    'end': ['2012-08-26 16:35:00'],
+    'Activity': ['Sleep'],
+}
+
+#Wake up for an hour
+sleep_break_anomaly = {
+    'start': ['2012-09-12 02:30:00'],
+    'end': ['2012-09-12 03:30:00'],
+    'Activity': ['Watch_TV',],
+}
+
+#Additional Sleep Anomalies EXCLUDED ------------------------------------------------------------
+
+
+#sleeps +5 hours longer - ALREADY EXISTS
 sleep_in_anomaly_singlevar = {
     'start': [
         '2012-08-18 06:32:00',
@@ -42,12 +93,6 @@ sleep_in_anomaly_multivar = {
         ],
 }
 
-#does not sleep day
-sleep_random_anomaly = {
-    'start': ['2012-08-26 12:17:00'],
-    'end': ['2012-08-26 16:41:00'],
-    'Activity': ['Sleep'],
-}
 
 #sleeps 3 hours later
 sleep_late_anomaly = {
@@ -116,14 +161,13 @@ eating_anomaly = {
         ],
 }
 
-#just for sleep
-anomalies = [
-    sleep_in_anomaly_singlevar,
+# *Exclude section complete* -----------------------------------------------------------------
+
+
+#synth_anomalies
+synth_anomalies = [
     sleep_random_anomaly,
-    sleep_late_anomaly,
-    no_sleep_anomaly,
-    # toilet_anomaly,
-    # eating_anomaly,
+    sleep_break_anomaly
 ]
 
 def anom_summary(anomaly):
@@ -167,11 +211,13 @@ def insert_anomaly(anomaly,df):
 def example_main():
     df = pd.read_csv(filename_proc)
     summary_anomalies = []
-    for anomaly in anomalies:
+    for anomaly in synth_anomalies:
         insert_anomaly(anomaly, df)
         summary_anomaly = anom_summary(anomaly)
         summary_anomalies.append(summary_anomaly)
     df_sum = pd.DataFrame(summary_anomalies, columns= ['start', 'end'])
+    df_known_sum = pd.DataFrame(existing_anomalies_hh101, columns= ['start', 'end'])
+    df_sum = df_sum.append(df_known_sum)
 
     df.to_csv(f"processed_data/anomalous/{data_name}/{data_name}_anomalous_{interval}sw.csv")
     df_sum.to_csv(f"processed_data/anomalous/{data_name}/{data_name}_anomalous_{interval}sw_summary.csv")
