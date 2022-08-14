f0c8f254e1d21a8f9e261239f8707e6a2da3ffae
Gen Hall <genbhall@gmail.com>
2022-08-14 16:16:42 +0100

    with model files

--- preprocessing/global_variables/global_variables.py
+++ preprocessing/global_variables/global_variables.py
@@ -1,12 +1,63 @@
 from datetime import datetime
 
-# variables to do with the file looked at
-filename = "../CASAS_dataset/hh101/hh101.ann.features.csv"
-filename_proc = "processed_data/hh101/hh101_preprocessed_60sw.csv"
-data_name = 'hh101'
+# hh101 variables
+# filename = "../CASAS_dataset/hh101/hh101.ann.features.csv"
+# filename_proc = "processed_data/hh101/hh101_preprocessed_60sw.csv"
+# data_name = 'hh101'
+# interval = 60
+# start_date = datetime(2012,7,20,0,0,0)
+# end_date = datetime(2012,9,17,0,0,0)
+# all_activities = [
+#     'timestamp', 
+#     'Step_Out', 
+#     'Other_Activity',
+#     'Toilet', 
+#     'Phone', 
+#     'Personal_Hygiene', 
+#     'Leave_Home', 
+#     'Enter_Home', 
+#     'Relax', 
+#     'Sleep_Out_Of_Bed', 
+#     'Drink', 
+#     'Watch_TV', 
+#     'Dress', 
+#     'Evening_Meds', 
+#     'Wake_Up', 
+#     'Read', 
+#     'Morning_Meds', 
+#     'Cook_Breakfast', 
+#     'Eat_Breakfast', 
+#     'Bathe', 
+#     'Cook_Lunch', 
+#     'Eat_Lunch', 
+#     'Wash_Lunch_Dishes', 
+#     'Go_To_Sleep', 
+#     'Sleep', 
+#     'Bed_Toilet_Transition', 
+#     'Wash_Breakfast_Dishes', 
+#     'Work_At_Table', 
+#     'Groom', 
+#     'Cook', 
+#     'Eat', 
+#     'Cook_Dinner', 
+#     'Eat_Dinner', 
+#     'Wash_Dinner_Dishes', 
+#     'Wash_Dishes', 
+#     'Entertain_Guests',
+# ]
+# sleep_activities = [
+#     'timestamp',
+#     'Date', 
+#     'Sleep', 
+# ]
+
+# hh111 variables
+filename = "../CASAS_dataset/hh111/hh111.ann.features.csv"
+filename_proc = "processed_data/hh111/hh111_preprocessed_60sw.csv"
+data_name = 'hh111'
 interval = 60
-start_date = datetime(2012,7,20,0,0,0)
-end_date = datetime(2012,9,17,0,0,0)
+start_date = datetime(2011,6,15,0,0,0)
+end_date = datetime(2011,8,14,0,0,0)
 all_activities = [
     'timestamp', 
     'Step_Out', 
