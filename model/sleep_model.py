51267a5ad612ce7f25ad31b78a0ab85e15d8fda4
Gen Hall <gbh21@vm-shell4.doc.ic.ac.uk>
2022-08-24 10:05:39 +0100

    with all hyperparam tuning

--- model/sleep_model.py
+++ model/sleep_model.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     #hyperparameter tuning
     hyperparameter_tuning(df_train, df_valid, ground_truth)
     
-    #test for distributions of top 5 models - saves outcomes in new file
+    # This is saved
+    # #test for distributions of top 5 models - saves outcomes in new file
     # model_distribution_tuning(df_train, df_valid, ground_truth)
     
