99d637fe324ffb99579f9e6d3145b52ad898ea5a
Gen Hall <genbhall@gmail.com>
2022-08-24 20:02:40 +0100

    final evaluation

--- model/global_variables/global_variables.py
+++ model/global_variables/global_variables.py
@@ -1,55 +1,9 @@
 from datetime import datetime
 
 # variables to do with the file looked at
-data_name = 'hh101'
-interval = 60
-start_date = datetime(2012,7,20,0,0,0)
-filename_train = (f"../preprocessing/processed_data/{data_name}/{data_name}_preprocessed_{interval}sw.csv")
-filename_test = (f"../preprocessing/processed_data/anomalous/{data_name}/{data_name}_anomalous_{interval}sw.csv")
-filename_summary = (f"../preprocessing/processed_data/anomalous/{data_name}/{data_name}_anomalous_{interval}sw_summary.csv")
-all_activities = [
-    'timestamp', 
-    # 'Step_Out', 
-    # 'Other_Activity',
-    # 'Toilet', 
-    # 'Phone', 
-    # 'Personal_Hygiene', 
-    # 'Leave_Home', 
-    # 'Enter_Home', 
-    # 'Relax', 
-    # 'Sleep_Out_Of_Bed', 
-    # 'Drink', 
-    # 'Watch_TV', 
-    # 'Dress', 
-    # 'Evening_Meds', 
-    # 'Wake_Up', 
-    # 'Read', 
-    # 'Morning_Meds', 
-    # 'Cook_Breakfast', 
-    # 'Eat_Breakfast', 
-    # 'Bathe', 
-    # 'Cook_Lunch', 
-    # 'Eat_Lunch', 
-    # 'Wash_Lunch_Dishes', 
-    # 'Go_To_Sleep', 
-    'Sleep', 
-    # 'Bed_Toilet_Transition', 
-    # 'Wash_Breakfast_Dishes', 
-    # 'Work_At_Table', 
-    # 'Groom', 
-    # 'Cook', 
-    # 'Eat', 
-    # 'Cook_Dinner', 
-    # 'Eat_Dinner', 
-    # 'Wash_Dinner_Dishes', 
-    # 'Wash_Dishes', 
-    # 'Entertain_Guests',
-]
-
-# # variables to do with the file looked at
-# data_name = 'hh120'
+# data_name = 'hh101'
 # interval = 60
-# start_date = datetime(2012,1,28,0,0,0)
+# start_date = datetime(2012,7,20,0,0,0)
 # filename_train = (f"../preprocessing/processed_data/{data_name}/{data_name}_preprocessed_{interval}sw.csv")
 # filename_test = (f"../preprocessing/processed_data/anomalous/{data_name}/{data_name}_anomalous_{interval}sw.csv")
 # filename_summary = (f"../preprocessing/processed_data/anomalous/{data_name}/{data_name}_anomalous_{interval}sw_summary.csv")
@@ -91,3 +45,49 @@ all_activities = [
 #     # 'Wash_Dishes', 
 #     # 'Entertain_Guests',
 # ]
+
+# variables to do with the file looked at
+data_name = 'hh120'
+interval = 60
+start_date = datetime(2012,1,28,0,0,0)
+filename_train = (f"../preprocessing/processed_data/{data_name}/{data_name}_preprocessed_{interval}sw.csv")
+filename_test = (f"../preprocessing/processed_data/anomalous/{data_name}/{data_name}_anomalous_{interval}sw.csv")
+filename_summary = (f"../preprocessing/processed_data/anomalous/{data_name}/{data_name}_anomalous_{interval}sw_summary.csv")
+all_activities = [
+    'timestamp', 
+    # 'Step_Out', 
+    # 'Other_Activity',
+    # 'Toilet', 
+    # 'Phone', 
+    # 'Personal_Hygiene', 
+    # 'Leave_Home', 
+    # 'Enter_Home', 
+    # 'Relax', 
+    # 'Sleep_Out_Of_Bed', 
+    # 'Drink', 
+    # 'Watch_TV', 
+    # 'Dress', 
+    # 'Evening_Meds', 
+    # 'Wake_Up', 
+    # 'Read', 
+    # 'Morning_Meds', 
+    # 'Cook_Breakfast', 
+    # 'Eat_Breakfast', 
+    # 'Bathe', 
+    # 'Cook_Lunch', 
+    # 'Eat_Lunch', 
+    # 'Wash_Lunch_Dishes', 
+    # 'Go_To_Sleep', 
+    'Sleep', 
+    # 'Bed_Toilet_Transition', 
+    # 'Wash_Breakfast_Dishes', 
+    # 'Work_At_Table', 
+    # 'Groom', 
+    # 'Cook', 
+    # 'Eat', 
+    # 'Cook_Dinner', 
+    # 'Eat_Dinner', 
+    # 'Wash_Dinner_Dishes', 
+    # 'Wash_Dishes', 
+    # 'Entertain_Guests',
+]
--- model/sleep_model.py
+++ model/sleep_model.py
@@ -1,10 +1,13 @@
 from supporting_func.supporting_func import split_data, convert_dfdatetotime
 from global_variables.global_variables import all_activities, filename_test, filename_summary
-from supporting_func.sleep_hyperparameter import hyperparameter_tuning, model_distribution_tuning
+from supporting_func.sleep_hyperparameter import hyperparameter_tuning, model_distribution_tuning, final_distribution_tuning
 
 import pandas as pd
 
 if __name__ == "__main__":
+    
+    print(filename_test)
+    print(filename_summary)
     
     #read and split the relevant files
     df = pd.read_csv(filename_test)
@@ -19,8 +22,11 @@ if __name__ == "__main__":
     #hyperparameter tuning
     # hyperparameter_tuning(df_train, df_valid, ground_truth)
     
-    # This is saved
-    #test for distributions of top 5 models - saves outcomes in new file
-    top_5_params = pd.read_csv("hyperparam_logs/sleep/F1_top_5.csv")
-    model_distribution_tuning(df_train, df_valid, ground_truth)
+    # # This is saved
+    # #test for distributions of top 5 models - saves outcomes in new file
+    # top_5_params = pd.read_csv("hyperparam_logs/sleep/F1_top_5.csv")
+    # model_distribution_tuning(df_train, df_valid, top_5_params, ground_truth)
     
+    #FINAL TEST FOR EVALUATION PURPOSES ON HH120
+    final_model_param = ['',100,1800,10,20,5,5]
+    final_distribution_tuning(df_train, df_valid, final_model_param, ground_truth)
